a1bbe9d0ece77681109404a9b1852407358ddd9b
OscarBarrera89 <oscarbar745504@gmail.com>
2024-11-13 10:34:11 +0100

    conexion base de datos

--- Restaurante/php/config.php
+++ Restaurante/php/config.php
@@ -1,18 +1,69 @@
-<?php 
-function obtenerConexion() {
-    // Establecer conexión y opciones de mysql
-    // Errores mysql sin excepciones
-    mysqli_report(MYSQLI_REPORT_OFF);
+<?php
+/*
+Este fichero hay que incluirlo en cada script del proyecto.
+De esta manera:
+ - podrás usar la información que aparece aquí en cualquier parte
+ - sólo necesitas actualizar este fichero para reflejar cualquier
+   cambio en toda la aplicación
+*/
 
-    // Importante, ajustar los siguientes parámetros
-    $conexion = new mysqli("localhost", "root", "test", "restaurante","8000");
-    // $conexion = mysqli_connect('db', 'root', 'test', "dbname");
-    mysqli_set_charset($conexion, 'utf8');
+/* Array asociativo con la configuración de conexión a la base de datos */
+$basedatos = array(
+    "basedatos" => "restaurante",
+    "usuario" => "root",
+    "password" => "test",
+    "servidor" => "localhost",
+    "puerto" => 8000
+);
 
-    if ($conexion->connect_error) {
-        die("Conexión fallida: " . $conn->connect_error);
+
+/* ERROR REPORTING */
+//Indica que sólo se mostrarán errores, no Warnings ni otros errores. 
+//Valores posibles: E_ERROR | E_WARNING | E_PARSE | E_NOTICE
+error_reporting(E_ERROR);
+
+// Reporte de errores para mysql sin excepciones
+mysqli_report(MYSQLI_REPORT_OFF);
+
+
+/* FUNCIONES COMUNES */
+
+function obtenerConexion()
+{
+    global $basedatos; //recuperamos el array con la conexión
+
+    $conexion = mysqli_connect($basedatos["servidor"], $basedatos["usuario"], $basedatos["password"], $basedatos["basedatos"]) or die(mysqli_error($conexion));
+    // $conexion = new mysqli($basedatos["servidor"], $basedatos["usuario"], $basedatos["password"], $basedatos["basedatos"], $basedatos["puerto"]) ;
+
+    if ($conexion->connect_errno) {
+        responder(null, true, "Error al conectar a la base de datos. \nCompruebe los parámetros de la conexión: " .
+            $conexion->connect_error . " Código de error: " . $conexion->connect_errno, $conexion);
     }
-    echo "Conectado con éxito";
 
+    mysqli_set_charset($conexion, "utf8");
+    //Si no se produjo un error devolvemos el objeto de la conexión
     return $conexion;
+}
+
+
+function responder($datos, $ok, $mensaje, $conexion)
+{
+    // Formatear array asociativo con los campos de la respuesta
+    $respuesta["ok"] = $ok; // Boolean true si OK -- false si error
+    $respuesta["datos"] = $datos;  // Datos devueltos
+    $respuesta["mensaje"] = $mensaje; // Información sobre la operación
+
+    //Mandamos la respuesta al cliente
+    echo json_encode($respuesta);
+
+    // Cerramos la conexión
+    mysqli_close($conexion);
+
+    if ($ok == false) {
+        // Finalizar el proceso el servidor con indicador de error
+        exit(1);
+    } else {
+        // Finalizar el proceso el servidor con indicador de éxito
+        exit(0);
+    }
 }
