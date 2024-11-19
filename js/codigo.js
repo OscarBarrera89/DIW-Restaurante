ca6866df05b931c79e0a637f95df9d01d2d551c0
OscarBarrera89 <oscarbar745504@gmail.com>
2024-11-19 18:41:52 +0100

    cambios Restaurante

--- js/codigo.js
+++ js/codigo.js
@@ -8,7 +8,39 @@ inicio();
 function inicio(){
     document.querySelector("#mnuAltaPedido").addEventListener("click",mostrarFormulario);
     document.querySelector("#mnuListadoPedidosCliente").addEventListener("click",mostrarFormulario);
+
+    frmAltaPedido.btnAltaPedido.addEventListener("click",procesarAltaPedido);
+    frmListadoPedidosCliente.btnListadoPedidosCliente.addEventListener("click",procesarListadoPedidos)
 }
+
+async function procesarListadoPedidos(){
+    let nombreCliente = frmListadoPedidosCliente.txtNombreClienteListado.value.trim();
+
+    const listado = await oBar.listadoPedidoCliente(nombreCliente);
+
+    document.querySelector("#listado").innerHTML = listado;
+    document.querySelector("#listado").classList.remove("d-none");
+
+}
+
+async function procesarAltaPedido(){
+    let nombre = frmAltaPedido.txtNombreCliente.value.trim();
+    let idplato = frmAltaPedido.lstPlatos.value;
+    let unidades = frmAltaPedido.rbtUnidades.value;
+
+    const pedido = new Pedido(null, nombre, idplato, unidades);
+
+    const respuesta = await oBar.altaPedido(pedido);
+
+    alert(respuesta.mensaje);
+
+    if(respuesta.ok){
+        frmAltaPedido.reset();
+        ocultarFormularios();
+    }
+
+}
+
 
 function mostrarFormulario(oEvento){
     ocultarFormularios();
@@ -25,13 +57,23 @@ function mostrarFormulario(oEvento){
     }
 }
 
-function cargarDesplegable(){
-    const platos = oRestaurante.getPlatos();
-    
+async function cargarDesplegable(){
+    const respuesta = await oRestaurante.getPlatos();
 
+    if(respuesta.ok){
+        let optionsPlatos = "";
+        for(let plato of respuesta.datos){
+            optionsPlatos += `<option value="${plato.idplato}">${plato.nombre}</option>`;
+        }
+
+        frmAltaPedido.lstPlatos.innerHTML  = optionsPlatos;
+    } else{
+        alert("Error al recuperar los platos");
+    }
 }
 
 function ocultarFormularios(){
     frmListadoPedidosCliente.classList.add("d-none");
     frmAltaPedido.classList.add("d-none");
+    document.querySelector("#listado").classList.add("d-none");
 }
--- js/objetos.js
+++ js/objetos.js
@@ -1,89 +1,247 @@
-"use strict";
+class Cliente {
+    #_idcliente;
+    #_nombre;
+    #_email;
+    #_telefono;
 
-class Pedido{
-    #idpedido;
-    #cliente;
-    #idplato;
-    #unidades;
-    constructor(idpedido, cliente, idplato, unidades){
-        this.#idpedido = idpedido;
-        this.#cliente = cliente;
-        this.#idplato = idplato;
-        this.#unidades = unidades;
-    }
-
-    get idpedido(){
-        return this.#idpedido;
-    }
-    get cliente(){
-        return this.#cliente;
-    }
-    get idplato(){
-        return this.#idplato;
-    }
-    get unidades(){
-        return this.#unidades;
+    constructor(idcliente, nombre, email, telefono) {
+        this.#_idcliente = idcliente;
+        this.#_nombre = nombre;
+        this.#_email = email;
+        this.#_telefono = telefono;
     }
 
-    set idpedido(valor){
-        this.#idpedido = valor;
+    // Getters
+    get idcliente() {
+        return this.#_idcliente;
     }
-    set cliente(valor){
-        this.#cliente = valor;
+    get nombre() {
+        return this.#_nombre;
     }
-    set idplato(valor){
-        this.#idplato = valor;
+    get email() {
+        return this.#_email;
     }
-    set unidades(valor){
-        this.#unidades = valor;
+    get telefono() {
+        return this.#_telefono;
+    }
+
+    // Setters
+    set idcliente(value) {
+        this.#_idcliente = value;
+    }
+    set nombre(value) {
+        this.#_nombre = value;
+    }
+    set email(value) {
+        this.#_email = value;
+    }
+    set telefono(value) {
+        this.#_telefono = value;
+    }
+
+    // Método para personalizar la salida JSON
+    toJSON() {
+        return {
+            idcliente: this.#_idcliente,
+            nombre: this.#_nombre,
+            email: this.#_email,
+            telefono: this.#_telefono
+        };
     }
 }
 
-class Plato {
-    #idplato;
-    #nombre;
-    #descripcion;
-    #precio;
-    constructor(idplato,nombre, descripcion, precio){
-        this.#idplato = idplato;
-        this.#nombre = nombre;
-        this.#descripcion = descripcion;
-        this.#precio = precio;
+class Detalle_pedido {
+    #_iddetalle;
+    #_idpedido;
+    #_idplato;
+    #_cantidad;
+
+    constructor(iddetalle, idpedido, idplato, cantidad) {
+        this.#_iddetalle = iddetalle;
+        this.#_idpedido = idpedido;
+        this.#_idplato = idplato;
+        this.#_cantidad = cantidad;
     }
 
-    get idplato(){
-        return this.#idplato;
+    // Getters
+    get iddetalle() {
+        return this.#_iddetalle;
     }
-    get nombre(){
-        return this.#nombre;
+    get idpedido() {
+        return this.#_idpedido;
     }
-    get descripcion(){
-        return this.#descripcion;
+    get idplato() {
+        return this.#_idplato;
     }
-    get precio(){
-        return this.#precio;
+    get cantidad() {
+        return this.#_cantidad;
     }
 
-    set idplato(valor){
-        this.#idplato = valor;
+    // Setters
+    set iddetalle(value) {
+        this.#_iddetalle = value;
     }
-    set nombre(valor){
-        this.#nombre = valor;
+    set idpedido(value) {
+        this.#_idpedido = value;
     }
-    set descripcion(valor){
-        this.#descripcion = valor;
+    set idplato(value) {
+        this.#_idplato = value;
     }
-    set precio(valor){
-        this.#precio = valor;
+    set cantidad(value) {
+        this.#_cantidad = value;
+    }
+
+    // Método para personalizar la salida JSON
+    toJSON() {
+        return {
+            iddetalle: this.#_iddetalle,
+            idpedido: this.#_idpedido,
+            idplato: this.#_idplato,
+            cantidad: this.#_cantidad
+        };
     }
 }
 
-class Bar{
-    altaPedido(oPedido){
+class Menu {
+    #_idplato;
+    #_nombre;
+    #_descripcion;
+    #_precio;
+    #_alergenos;
 
+    constructor(idplato, nombre, descripcion, precio, alergenos) {
+        this.#_idplato = idplato;
+        this.#_nombre = nombre;
+        this.#_descripcion = descripcion;
+        this.#_precio = precio;
+        this.#_alergenos = alergenos;
     }
 
-    async getPlatos(){
+    // Getters
+    get idplato() {
+        return this.#_idplato;
+    }
+    get nombre() {
+        return this.#_nombre;
+    }
+    get descripcion() {
+        return this.#_descripcion;
+    }
+    get precio() {
+        return this.#_precio;
+    }
+    get alergenos() {
+        return this.#_alergenos;
+    }
+
+    // Setters
+    set idplato(value) {
+        this.#_idplato = value;
+    }
+    set nombre(value) {
+        this.#_nombre = value;
+    }
+    set descripcion(value) {
+        this.#_descripcion = value;
+    }
+    set precio(value) {
+        this.#_precio = value;
+    }
+    set alergenos(value) {
+        this.#_alergenos = value;
+    }
+
+    // Método para personalizar la salida JSON
+    toJSON() {
+        return {
+            idplato: this.#_idplato,
+            nombre: this.#_nombre,
+            descripcion: this.#_descripcion,
+            precio: this.#_precio,
+            alergenos: this.#_alergenos
+        };
+    }
+}
+
+class Pedido {
+    #_idpedido;
+    #_idcliente;
+    #_fecha;
+    #_camarero;
+    #_total;
+
+    constructor(idpedido, idcliente, fecha, camarero, total) {
+        this.#_idpedido = idpedido;
+        this.#_idcliente = idcliente;
+        this.#_fecha = fecha;
+        this.#_camarero = camarero;
+        this.#_total = total;
+    }
+
+    // Getters
+    get idpedido() {
+        return this.#_idpedido;
+    }
+    get idcliente() {
+        return this.#_idcliente;
+    }
+    get fecha() {
+        return this.#_fecha;
+    }
+    get camarero() {
+        return this.#_camarero;
+    }
+    get total() {
+        return this.#_total;
+    }
+
+    // Setters
+    set idpedido(value) {
+        this.#_idpedido = value;
+    }
+    set idcliente(value) {
+        this.#_idcliente = value;
+    }
+    set fecha(value) {
+        this.#_fecha = value;
+    }
+    set camarero(value) {
+        this.#_camarero = value;
+    }
+    set total(value) {
+        this.#_total = value;
+    }
+
+    // Método para personalizar la salida JSON
+    toJSON() {
+        return {
+            idpedido: this.#_idpedido,
+            idcliente: this.#_idcliente,
+            fecha: this.#_fecha,
+            camarero: this.#_camarero,
+            total: this.#_total
+        };
+    }
+}
+
+class Restaurante{
+    async altaPedido(oPedido) {
+        let datos = new FormData();
+
+        // datos.append("cliente", oPedido.cliente);
+        // datos.append("idplato", oPedido.idplato);
+        // datos.append("unidades", oPedido.unidades);
+
+        datos.append("pedido", JSON.stringify(oPedido));
+
+        console.log("Datos:", datos);
+
+        let respuesta = await peticionPOST("alta_pedido.php", datos);
+
+        return respuesta;
+    }
+
+    async getPlatos() {
         let datos = new FormData();
 
         let respuesta = await peticionGET("get_platos.php", datos);
@@ -91,7 +249,32 @@ class Bar{
         return respuesta;
     }
 
-    listadoPedidoCliente(nombreCliente){
+    async listadoPedidoCliente(nombreCliente) {
+        const datos = new FormData();
 
+        datos.append("nombre", nombreCliente);
+
+        const respuesta = await peticionGET("listado_pedidos.php", datos);
+
+        if (respuesta.ok) {
+            let listado = `<h1>Pedidos de ${nombreCliente}</h1>`;
+
+            listado += "<table class='table table-hover table-striped'>";
+            listado += "<thead><tr><th>IDPEDIDO</th><th>PLATO</th><th>UNIDADES</th>";
+            listado += "<th>PRECIO</th><th>TOTAL</th></tr></thead><tbody>";
+
+            for (let pedido of respuesta.datos) {
+                listado += `<tr><td>${pedido.idpedido}</td>`;
+                listado += `<td>${pedido.nombre}</td>`;
+                listado += `<td>${pedido.unidades}</td>`;
+                listado += `<td>${pedido.precio}</td>`;
+                listado += `<td>${pedido.unidades * pedido.precio}</td></tr>`;
+            }
+            listado += "</tbody></table>";
+
+            return listado;
+        } else {
+            return "<h1>Error al recuperar los datos</h1>"
+        }
     }
 }
